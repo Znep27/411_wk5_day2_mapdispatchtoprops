fdc618e2f1991b8e447dc163d4f5b12cbf3fe48d
Marc Rozenburgh <znep27@hotmail.com>
2023-11-16 20:47:55 -0600

    update

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 import Dashboard from '../components/Dashboard'
-// import { removeCar } action here
+import { addCar, removeCar } from '../redux/actions'
 
 const mapStateToProps = (state) => {
     return {
@@ -10,5 +10,11 @@ const mapStateToProps = (state) => {
 }
 
 // add mapDispatchToProps function here
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addCar: (car) => dispatch(addCar(car)),
+        removeCar: (index) => dispatch(removeCar(index))
+    }
+}
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
